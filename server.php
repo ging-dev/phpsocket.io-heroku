8b80af398eb7d482fd4a4e6f9f9261b1395876b5
ging-dev <gingdz11012001@gmail.com>
2021-01-15 20:38:41 +0700

    Update for Vinarum - check post chat - rebase commit

--- server.php
+++ server.php
@@ -8,8 +8,23 @@ $io = new SocketIO(getenv('PORT'));
 $io->origins(getenv('DOMAIN'));
 
 $io->on('connection', function ($socket) use ($io) {
-    $socket->on('demo', function ($data) use ($io) {
-        $io->emit('demo', $data);
+    $socket->on('online', function ($name) use ($socket) {
+        if (!isset($socket->name)) {
+            $socket->name = $name;
+            $socket->broadcast->emit('online', [$socket->name.' đã tham gia', 'success']);
+        }
+    });
+    $socket->on('chat', function ($msg) use ($socket) {
+        if (is_array($msg) && array_keys($msg) == ['data', 'signature']) {
+            [$data, $signature] = array_values($msg);
+            $hash = hash_hmac('md2', json_encode($data), getenv('SECRET'));
+            if ($signature == $hash) {
+                $socket->broadcast->emit('chat', $data);
+            }
+        }
+    });
+    $socket->on('disconnect', function () use ($socket) {
+        $socket->broadcast->emit('online', [$socket->name.' đã thoát', 'error']);
     });
 });
 
