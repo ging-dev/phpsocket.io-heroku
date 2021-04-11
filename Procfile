e61211e1f71130b7047f4c52101da374be0f96f8
ging-dev <gingdz11012001@gmail.com>
2021-04-11 18:03:11 +0700

    Update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: php server.php start
+web: vendor/bin/heroku-php-apache2 -C httpd.conf public/ & php server.php start & wait -n
--- server.php
+++ server.php
@@ -2,10 +2,25 @@
 
 use Workerman\Worker;
 use PHPSocketIO\SocketIO;
+use Workerman\Protocols\Http\Request;
+use Workerman\Connection\TcpConnection;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-$io = new SocketIO(getenv('PORT'));
+$io = new SocketIO(3000);
 $io->origins(getenv('DOMAIN'));
+
+$io->on('workerStart', function() use ($io) {
+    $inner_http_worker = new Worker('http://0.0.0.0:3001');
+    $inner_http_worker->onMessage = function(TcpConnection $http_connection, Request $request) use ($io) {
+        if(!isset($request->get()['message'])) {
+            return $http_connection->send('Fail, "message" not found');
+        }
+        $io->emit('online', [$request->get('message'), 'success']);
+        $http_connection->send('OK');
+    };
+    $inner_http_worker->listen();
+});
 
 $io->on('connection', function ($socket) use ($io) {
     $socket->on('online', function ($name) use ($socket) {
